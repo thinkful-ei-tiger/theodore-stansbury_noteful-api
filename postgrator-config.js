59a7022d2bf3fe87104adc50bc9772c9de155fa1
Theodore Stansbury <tstansbu.27@gmail.com>
2021-01-08 16:33:09 -0500

    changed console log on app.js

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,6 +5,6 @@ module.exports = {
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : 'postgres://qifxpiblfepykg:685d95ebab20dcabcc9de4800edd6c3f6bae536a1ed0e5db6202c4d03e78dfcc@ec2-52-203-49-58.compute-1.amazonaws.com:5432/d2nc8vlisg88h0',
+    : process.env.DATABASE_URL,
     "ssl": process.env.NODE_ENV === 'production',
 }
--- src/app.js
+++ src/app.js
@@ -26,13 +26,12 @@ app.get('/', (req, res)=>{
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    console.log(error)
     if (NODE_ENV === 'production') {
         response = {error: {message: 'server error'}}
     } else {
-        console.error(error)
         response = {message: error.message, error}
     }
+    console.error(error)
     res.status(500).json(response)
 })
 
